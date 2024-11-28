042724e26456935291dd99197bc79497d31db3c9
Miguel Cardoso <joaomiguel.cardoso2006@outlook.com>
2024-11-28 02:45:14 -0300

    já estava surtando kkkkkk

--- src/components/ui/buttonLogut.jsx
+++ src/components/ui/buttonLogut.jsx
@@ -4,13 +4,10 @@ import { Button } from "./button";
 import { useRouter } from "next/navigation";
 
 export default function ButtonLogut() {
-  const router = useRouter();
   async function logout() {
     await signOut({
-      redirect: false,
+      redirect: true,
     });
-
-    router.replace("/");
   }
   return (
     <Button onClick={logout} variant="destructive" className="px-10">
